ef62ec1fda185f1801c72e3b224f8cc666585c9b
Mybread2 <ckwnsgh4@naver.com>
2025-04-16 20:06:13 +0900

    2단계 계산기 구현 완료 사칙연산 기호를 잘못 입력했을때 다시 입력받는 곳으로 이동하는 것 수정해야한다

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -1,16 +1,20 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
+
+        Calculator calc = new Calculator(); // Calculator 인스턴스 생성
+
+        List<Double> newResults = new ArrayList<Double>(); // App 클래스에서 연산 결과를 관리할 수 있는 기능
 
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            boolean possible = true; // boolean은 연산 처리와 오류 방지 로직을 깔끔하게 제어하기 위한 장치로써 사용했다.
-                                     // 다음 계산시에 true로 다시 바꿔주기 위해 반복문 맨 윗줄에 적어주었다.
-
+        inputLoop : while (true) {
             System.out.print("첫 번째 양의 정수를 입력하세요: ");
             int positiveInteger1 = sc.nextInt();
 
@@ -19,50 +23,32 @@ public class App {
 
             System.out.print("사칙연산 기호를 입력하세요 : ");
             char operator = sc.next().charAt(0);
+            sc.nextLine();
 
-            double result = 0; // 나눗셈에서 소숫점까지 표현해주기 위해 double로 표현
+            calc.calculate(positiveInteger1, positiveInteger2, operator); // 생성한 인스턴스 활용
 
-            switch (operator) {
-                case '+':
-                    result = positiveInteger1 + positiveInteger2;
+            secondLoop :while (true) {
+                System.out.println("다음 계산을 하고싶다면 '계산'을 입력 / 삭제를 원하면 '삭제'를 입력 / 계산기를 종료하고싶다면 'exit' 입력");
+
+
+                String inputExit = sc.nextLine(); // 단어를 입력받을 준비
+
+
+                if (inputExit.equals("exit")) {
+                    System.out.println("계산기를 종료합니다.");
                     break;
-
-                case '-':
-                    result = positiveInteger1 - positiveInteger2;
-                    break;
-
-                case '*':
-                    result = positiveInteger1 * positiveInteger2;
-                    break;
-
-                case '/':
-                    if (positiveInteger2 == 0) {
-                        System.out.println("0으로 나눌 수 없습니다.");
-                        System.out.println(); // 줄을 한 칸 띄어주기 위해
-                        continue; // 두 번째 양의 정수가 0이더라도 계산결과를 나타내주는 것을 방지하기 위해 현재 반복을 건너뛰게 해주었다.
-                    } else {
-                        result = (double) positiveInteger1 / positiveInteger2;
-                    }
-                    break;
-
-                default:
-                    System.out.println("올바른 사칙연산 기호를 입력해주세요.");
-                    possible = false;
+                } else if (inputExit.equals("삭제")) {
+                    calc.removeResults();
+                } else if (inputExit.equals("계산")) {
+                    continue inputLoop;
+                } else {
+                    System.out.println("올바른 입력 값이 아닙니다.");
+                }
             }
-
-            if (possible){
-                System.out.println("결과: " + result);
-            }
-
-            System.out.print("엔터키를 누르면 다음 계산으로 넘어갑니다. (exit 입력 시 종료)");
-            sc.nextLine(); // 엔터키를 통해 다음 계산으로 부드럽게 넘어갈 수 있다.
-            String inputExit = sc.nextLine(); // 단어를 입력받을 준비
-            if (inputExit.equals("exit")){
-                System.out.println("계산기를 종료합니다.");
-                break;
-            }
-            System.out.println(); // 다음 계산으로 넘어갈 때 한칸 띄어주기 위해
+            sc.close();
         }
-        sc.close();
+
+        }
+
+
     }
-}
---
+++ src/calculator/Calculator.java
@@ -0,0 +1,79 @@
+package calculator;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    private List<Double> results = new ArrayList<Double>(); // 연산 결과들을 저장하는 컬렉션 타입 필드
+
+    public List<Double> getResults() { // getter 메서드
+        return this.results;           // results는 연산 결과들을 저장하는 리스트이고 getResults()는 그 리스트를 읽어올 수 있게 해주는 메서드
+    }
+
+    public void setResults(List<Double> results) { // setter 메서드
+        this.results = results;
+    }
+
+    public void calculate(int positiveInteger1, int positiveInteger2, char operator) {
+        // App에서 입력받은 데이터 3개를 가져오는 기능
+
+        boolean possible = true; // boolean은 연산 처리와 오류 방지 로직을 깔끔하게 제어하기 위한 장치로써 사용했다.
+                                 // 사칙연산 기능을 Calculator 클래스에 만들어주었고, 반복문은 App 클래스에 구현이 되어있어서
+                                 // 이 클래스 내에는 continue를 사용하지 못한다. 따라서 조건문을 통해 오류 로직을 제어하기 위해 사용했다.
+
+        double result = 0; // 나눗셈에서 소숫점까지 표현해주기 위해 double로 표현
+
+        switch (operator) {
+            case '+':
+                result = positiveInteger1 + positiveInteger2;
+                break;
+
+            case '-':
+                result = positiveInteger1 - positiveInteger2;
+                break;
+
+            case '*':
+                result = positiveInteger1 * positiveInteger2;
+                break;
+
+            case '/':
+                if (positiveInteger2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    possible = false;
+                    System.out.println(); // 줄을 한 칸 띄어주기 위해
+                } else {
+                    result = (double) positiveInteger1 / positiveInteger2;
+                }
+                break;
+
+            default:
+                System.out.println("올바른 사칙연산 기호를 입력해주세요.");
+                System.out.println(); // 줄을 한 칸 띄어주기 위해
+                possible = false;
+        }
+
+        if (possible) {
+            System.out.println("결과: " + result);
+
+            results.add(result); // results는 List<Double> 타입의 리스트이다.
+                                 // result는 연산이 끝난 후에 얻은 데이터 값이다.
+                                 // 그 result 값을 results 리스트에 추가하겠다는 의미
+        }
+    }
+        public void removeResults() { // 기록된 값 맨 첫 번째 삭제하는 메서드
+             if(!results.isEmpty()) {
+                 double removed = results.remove(0);
+                 System.out.println("삭제된 값 : " + removed);
+                 for(int i=0; i<results.size(); i++) {
+                     System.out.println((i+1) + "번째 결과값 : " + results.get(i));
+                 }
+             } else {
+                 System.out.println("삭제할 연산 결과가 존재하지 않습니다.");
+             }
+    }
+    }
+
+
+
